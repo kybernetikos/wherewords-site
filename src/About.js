2242b1cb0e901290a443a605ab7d62ff1cdbe82b
kybernetikos <me@kybernetikos.com>
2021-01-04 08:35:57 +0000

    Improved about page.

--- src/About.js
+++ src/About.js
@@ -6,6 +6,18 @@ export function About() {
         <p>I made it as a fun holiday side project over a few days at the
         beginning of 2021.  The code is available <a href="https://github.com/kybernetikos/wherewords">on github</a>.</p>
 
+        <p>Things still to do:
+        </p>
+        <ul>
+            <li>Make it work off-line, as an app</li>
+            <li>Allow searching for locations</li>
+            <li>Better sharing of wherewords</li>
+            <li>Much better manual entry of the words</li>
+            <li>Better maps.  Maybe vectors?  Maybe for-pay?</li>
+            <li>Publish wherewords to npm</li>
+            <li>Pretty icons</li>
+        </ul>
+
         <p>
             There are three key parts to a project like this.
             <ol>
@@ -17,15 +29,23 @@ export function About() {
 
         <h3>The Word List</h3>
 
-        <p>
-            I ended up spending way more time on the word list than I expected.  I started by thinking
-            I was very clever in using the
-            <a href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki#Wordlist">BIP39 wordlist</a> which
-            has avoided similar words and requires only the first four letters to unambiguously
-            identify the word.  This wordlist has been carefully considered and is widely used by the
-            cryptocurrency community to store seeds for wallets, e.g. for safe storage of bitcoin or
-            ethereum.
-        </p>
+        <p>Turning data from one form into another is very much like changing the base of a number.
+        When people started using base 16, they didn't have enough numeric symbols to represent the
+        16 possible 'digits' that could be in each place, so they reused letters from the alphabet.
+        That's why 0x0A in hexadecimal is 10 in decimal.  Now what if you wanted to work in bigger
+        bases?  Well, you can reuse letters, first uppercase then lower and a few other symbols
+        and that lets you get to base 64 very easily, but beyond that you're starting to run out of
+        symbols.  What you can do then is start treating each word as a symbol (so 156 in decimal
+        becomes [1, 5, 6] and then could get turned into 9c in hexadecimal or [9, c] or could be
+        be turned into the 156th word of a wordlist  and be maybe [carrot].</p>
+
+        <p>I ended up spending way more time on the word list than I expected.  I started by thinking
+        I was very clever in using the
+        <a href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki#Wordlist">BIP39 wordlist</a> which
+        has avoided similar words and requires only the first four letters to unambiguously
+        identify the word.  This wordlist has been carefully considered and is widely used by the
+        cryptocurrency community to store seeds for wallets, e.g. for safe storage of bitcoin or
+        ethereum.</p>
 
         <p>Unfortunately, with only 2048 words, the bip39 wordlist can only provide 11 bits.  This
         meant that to get down to a decent accuracy using the geographic encoding I'd chosen, I
@@ -64,7 +84,7 @@ export function About() {
             great fun because they do something seemingly impossible - they map a 2d surface or even
             a 3d volume into a single 1d line. I first came across them in XKCD's map of the IPv4
             address space.
-            <img src="https://imgs.xkcd.com/comics/map_of_the_internet.jpg"/>
+            <img style={{width: "70vw"}} src="https://imgs.xkcd.com/comics/map_of_the_internet.jpg"/>
         </p>
 
         <p>
@@ -83,6 +103,7 @@ export function About() {
         </p>
 
         <h3>The Website</h3>
+        <p>The code for the website is available <a href="https://github.com/kybernetikos/wherewords-site">on github</a>.</p>
         <p>
             I used create-react-app to create the site, react-leaflet for the maps and nginx on
             caprover to easily serve it all from a cheap virtual private server.
